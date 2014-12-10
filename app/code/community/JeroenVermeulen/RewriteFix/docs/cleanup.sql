1c727d645b7d8fb1e1f401c160639ae41542fa74
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2014-12-10 17:35:53 +0100

    Improved cleanup HTML for shops using .html URLs

--- app/code/community/JeroenVermeulen/RewriteFix/docs/cleanup.sql
+++ app/code/community/JeroenVermeulen/RewriteFix/docs/cleanup.sql
@@ -3,11 +3,19 @@
 -- Execute:
 
 DELETE FROM `core_url_rewrite`
-WHERE `request_path` REGEXP '\-[[:digit:]]+$'
-  AND `options` = 'RP'
+WHERE
+  `options` = 'RP'
   AND `product_id` IS NOT NULL
-  AND   SUBSTRING( `request_path`, 1, LENGTH(`request_path`)-5 )
-      = SUBSTRING( `target_path`, 1, LENGTH(`target_path`)-5 )
+  AND
+  (
+    ( `request_path` REGEXP '\-[[:digit:]]+$'
+      AND SUBSTRING(`request_path`, 1, LENGTH(`request_path`) - 5) = SUBSTRING(`target_path`, 1, LENGTH(`target_path`) - 5)
+    )
+    OR
+    ( `request_path` REGEXP '\-[[:digit:]]+\.html?$'
+      AND SUBSTRING(`request_path`, 1, LENGTH(`request_path`) - 10) = SUBSTRING(`target_path`, 1, LENGTH(`target_path`) - 10)
+    )
+  )
 
 -- Reindex "Catalog URL Rewrites":
 -- $   screen php ~/httpdocs/shell/indexer.php -- --reindex catalog_url
