f28fdb09b2e49385d2ae1f13d6bf5528b9447690
united on air.magehost.pro <united@air.magehost.pro>
2015-10-20 22:27:57 +0200

    Improved cleanup script, can now handle big cleanups without using a huge amount of memory.

--- shell/jv_rewrite_cleanup.php
+++ shell/jv_rewrite_cleanup.php
@@ -12,43 +12,48 @@ require_once 'abstract.php';
  */
 class Mage_Shell_RewriteCleanup extends Mage_Shell_Abstract
 {
+    protected $writeAdapter;
+    protected $table;
+
+    /** 
+     * Constructor, prepare database stuff.
+     */
+    public function _construct() {
+            $this->writeAdapter = Mage::getSingleton('core/resource')->getConnection('core_write');
+            $this->table = Mage::getResourceModel('core/url_rewrite')->getMainTable();
+    }
 
     /**
      * Run script
      */
     public function run()
     {
-        if ($this->getArg('cleanup')) {
-            $table = Mage::getResourceModel('core/url_rewrite')->getMainTable();
-            $writeAdapter = Mage::getSingleton('core/resource')->getConnection('core_write');
+        if ( $this->getArg('cleanup') ) {
             $sql = sprintf( " SELECT `url_rewrite_id`, `request_path`, `target_path`
                               FROM %s
                               WHERE `options` = 'RP'
                               AND `product_id` IS NOT NULL
                               AND id_path LIKE '%%_%%' ",
-                            $writeAdapter->quoteIdentifier($table) );
+                            $this->writeAdapter->quoteIdentifier($this->table) );
             /** @var Varien_Db_Statement_Pdo_Mysql $stmt */
-            $stmt = $writeAdapter->query( $sql );
+            $stmt = $this->writeAdapter->query( $sql );
             $pregFilter = '/\-\d+/';
+            $deleteCount = 0;
             $deleteList = array();
             while ( $row = $stmt->fetch() ) {
                 if ( preg_replace($pregFilter,'',$row['request_path']) == preg_replace($pregFilter,'',$row['target_path']) ) {
-                    $deleteList[] = $row['url_rewrite_id'];
+                    $deleteList[] = intval( $row['url_rewrite_id'] );
+                }
+                if ( 5000 <= count($deleteList) ) {
+                    $deleteCount += $this->cleanRewrites( $deleteList );
+                    $deleteList = array();
                 }
             }
-            if ( empty($deleteList) ) {
-                echo "Congratulations, found no records to clean.\n";
+            $deleteCount += $this->cleanRewrites( $deleteList );
+            if ( $deleteCount ) {
+                printf( "\nCleaned up %d records.\n", $deleteCount );
             } else {
-                printf( "Cleaning up %d records...\n", count( $deleteList ) );
-                $chunks = array_chunk( $deleteList, 100 );
-                foreach ($chunks as $chunk) {
-                    $sql = sprintf(
-                        'DELETE FROM %s WHERE `url_rewrite_id` IN (%s)',
-                        $writeAdapter->quoteIdentifier( $table ),
-                        $writeAdapter->quote( $chunk )
-                    );
-                    $writeAdapter->query( $sql );
-                }
+                echo "Congratulations, found no records to clean.\n";
             }
             echo "Done.\n";
         } else {
@@ -58,7 +63,6 @@ class Mage_Shell_RewriteCleanup extends Mage_Shell_Abstract
 
     /**
      * Retrieve Usage Help Message
-     *
      */
     public function usageHelp()
     {
@@ -76,6 +80,29 @@ Usage:  php -f jv_rewrite_cleanup.php -- [options]
 
 USAGE;
     }
+
+    /**
+     * Delete a list of ids from the `core_url_rewrite` table
+     *
+     * @param $deleteList int[] - List of ids to delete
+     * @return int              - Nr of ids deleted
+     */
+    protected function cleanRewrites( $deleteList ) {
+	$count = count( $deleteList );
+        if ( $count ) {
+            $chunks = array_chunk( $deleteList, 100 );
+            foreach ($chunks as $chunk) {
+                $sql = sprintf( 'DELETE FROM %s WHERE `url_rewrite_id` IN (%s)',
+                                $this->writeAdapter->quoteIdentifier( $this->table ),
+                                $this->writeAdapter->quote( $chunk ) );
+                $this->writeAdapter->query( $sql );
+            }
+	}
+        echo ".";
+        flush();
+	return $count;
+    }
+
 }
 
 $shell = new Mage_Shell_RewriteCleanup();
