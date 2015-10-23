43200311aa9f621b5ae4db74305cee0f9a3e79c6
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2015-10-23 12:59:06 +0200

    Rewritefix: in Magento < 1.7 do not change the behaviour of the indexer, because our code is not compatible.

--- app/code/community/JeroenVermeulen/RewriteFix/Model/Catalog/Url.php
+++ app/code/community/JeroenVermeulen/RewriteFix/Model/Catalog/Url.php
@@ -25,52 +25,56 @@ class JeroenVermeulen_RewriteFix_Model_Catalog_Url extends Mage_Catalog_Model_Ur
      */
     public function getCategoryRequestPath($category, $parentPath)
     {
-        $storeId = $category->getStoreId();
-        $idPath  = $this->generatePath('id', null, $category);
-        $suffix  = $this->getCategoryUrlSuffix($storeId);
+        if ( version_compare( Mage::getVersion(), '1.7.0.0', '<' ) ) {
+            // The fix below is not compatible withR older Magento versions.
+            // Then only the shell command of this extension is useful.
+            return parent::getCategoryRequestPath($category, $parentPath);
+        } else {
+            $storeId = $category->getStoreId();
+            $idPath  = $this->generatePath( 'id', null, $category );
+            $suffix  = $this->getCategoryUrlSuffix( $storeId );
 
-        if (isset($this->_rewrites[$idPath])) {
-            $this->_rewrite = $this->_rewrites[$idPath];
-            $existingRequestPath = $this->_rewrites[$idPath]->getRequestPath();
-        }
+            if (isset( $this->_rewrites[ $idPath ] )) {
+                $this->_rewrite = $this->_rewrites[ $idPath ];
+                $existingRequestPath = $this->_rewrites[ $idPath ]->getRequestPath();
+            }
 
-        if ($category->getUrlKey() == '') {
-            $urlKey = $this->getCategoryModel()->formatUrlKey($category->getName());
-        }
-        else {
-            $urlKey = $this->getCategoryModel()->formatUrlKey($category->getUrlKey());
-        }
+            if ($category->getUrlKey() == '') {
+                $urlKey = $this->getCategoryModel()->formatUrlKey( $category->getName() );
+            } else {
+                $urlKey = $this->getCategoryModel()->formatUrlKey( $category->getUrlKey() );
+            }
 
-        $categoryUrlSuffix = $this->getCategoryUrlSuffix($category->getStoreId());
-        if (null === $parentPath) {
-            $parentPath = $this->getResource()->getCategoryParentPath($category);
-        }
-        elseif ($parentPath == '/') {
-            $parentPath = '';
-        }
-        $parentPath = Mage::helper('catalog/category')->getCategoryUrlPath($parentPath,
+            $categoryUrlSuffix = $this->getCategoryUrlSuffix( $category->getStoreId() );
+            if (null === $parentPath) {
+                $parentPath = $this->getResource()->getCategoryParentPath( $category );
+            } elseif ($parentPath == '/') {
+                $parentPath = '';
+            }
+            $parentPath = Mage::helper('catalog/category')->getCategoryUrlPath($parentPath,
                                                                            true, $category->getStoreId());
 
-        $requestPath = $parentPath . $urlKey . $categoryUrlSuffix;
+            $requestPath = $parentPath . $urlKey . $categoryUrlSuffix;
 
-        // PATCH TO FIX MAGENTO BUG THAT DUPLICATES REWRITES WHEN URL_KEY IS EQUAL BETWEEN CATEGORIES
-        $pattern = '/^' . preg_quote($parentPath.$urlKey, '/') . '-\d{1,}' . preg_quote($categoryUrlSuffix, '/')  . '$/';
-        if(isset($existingRequestPath) && (preg_match($pattern, $existingRequestPath) || $requestPath === $existingRequestPath)) {
-            return $existingRequestPath;
+            // PATCH TO FIX MAGENTO BUG THAT DUPLICATES REWRITES WHEN URL_KEY IS EQUAL BETWEEN CATEGORIES
+            $pattern = '/^' . preg_quote($parentPath.$urlKey, '/') . '-\d{1,}' . preg_quote($categoryUrlSuffix, '/')  . '$/';
+            if(isset($existingRequestPath) && (preg_match($pattern, $existingRequestPath) || $requestPath === $existingRequestPath)) {
+                return $existingRequestPath;
+            }
+            // END OF PATCH
+
+            if (isset( $existingRequestPath ) && $existingRequestPath == $requestPath . $suffix) {
+                return $existingRequestPath;
+            }
+
+            if ($this->_deleteOldTargetPath( $requestPath, $idPath, $storeId )) {
+                return $requestPath;
+            }
+
+            return $this->getUnusedPath($category->getStoreId(), $requestPath,
+                                        $this->generatePath( 'id', null, $category )
+            );
         }
-        // END OF PATCH
-
-        if (isset($existingRequestPath) && $existingRequestPath == $requestPath . $suffix) {
-            return $existingRequestPath;
-        }
-
-        if ($this->_deleteOldTargetPath($requestPath, $idPath, $storeId)) {
-            return $requestPath;
-        }
-
-        return $this->getUnusedPath($category->getStoreId(), $requestPath,
-                                    $this->generatePath('id', null, $category)
-        );
     }
 
     /**
@@ -82,99 +86,91 @@ class JeroenVermeulen_RewriteFix_Model_Catalog_Url extends Mage_Catalog_Model_Ur
      */
     public function getProductRequestPath($product, $category)
     {
-        if ($product->getUrlKey() == '') {
-            $urlKey = $this->getProductModel()->formatUrlKey($product->getName());
+        if ( version_compare( Mage::getVersion(), '1.7.0.0', '<' ) ) {
+            // The fix below is not compatible with older Magento versions.
+            // Then only the shell command of this extension is useful.
+            return parent::getProductRequestPath($product, $category);
         } else {
-            $urlKey = $this->getProductModel()->formatUrlKey($product->getUrlKey());
-        }
-        $storeId = $category->getStoreId();
-        $suffix  = $this->getProductUrlSuffix($storeId);
-        $idPath  = $this->generatePath('id', $product, $category);
-        /**
-         * Prepare product base request path
-         */
-        if ($category->getLevel() > 1) {
-            // To ensure, that category has path either from attribute or generated now
-            $this->_addCategoryUrlPath($category);
-            $categoryUrl = Mage::helper('catalog/category')->getCategoryUrlPath($category->getUrlPath(),
-                false, $storeId);
-            $requestPath = $categoryUrl . '/' . $urlKey;
-        } else {
-            $requestPath = $urlKey;
-        }
-
-        if (strlen($requestPath) > self::MAX_REQUEST_PATH_LENGTH + self::ALLOWED_REQUEST_PATH_OVERFLOW) {
-            $requestPath = substr($requestPath, 0, self::MAX_REQUEST_PATH_LENGTH);
-        }
-
-        $this->_rewrite = null;
-        /**
-         * Check $requestPath should be unique
-         */
-        if (isset($this->_rewrites[$idPath])) {
-            $this->_rewrite = $this->_rewrites[$idPath];
-            $existingRequestPath = $this->_rewrites[$idPath]->getRequestPath();
-
-            if ($existingRequestPath == $requestPath . $suffix) {
-                return $existingRequestPath;
+            if ($product->getUrlKey() == '') {
+                $urlKey = $this->getProductModel()->formatUrlKey( $product->getName() );
+            } else {
+                $urlKey = $this->getProductModel()->formatUrlKey( $product->getUrlKey() );
+            }
+            $storeId = $category->getStoreId();
+            $suffix  = $this->getProductUrlSuffix( $storeId );
+            $idPath  = $this->generatePath( 'id', $product, $category );
+            /**
+             * Prepare product base request path
+             */
+            if ($category->getLevel() > 1) {
+                // To ensure, that category has path either from attribute or generated now
+                $this->_addCategoryUrlPath( $category );
+                $categoryUrl = Mage::helper('catalog/category')->getCategoryUrlPath($category->getUrlPath(),
+                                                                                    false, $storeId);
+                $requestPath = $categoryUrl . '/' . $urlKey;
+            } else {
+                $requestPath = $urlKey;
             }
 
-            $existingRequestPath = preg_replace('/' . preg_quote($suffix, '/') . '$/', '', $existingRequestPath);
+            if (strlen( $requestPath ) > self::MAX_REQUEST_PATH_LENGTH + self::ALLOWED_REQUEST_PATH_OVERFLOW) {
+                $requestPath = substr( $requestPath, 0, self::MAX_REQUEST_PATH_LENGTH );
+            }
+
+            $this->_rewrite = null;
             /**
-             * Check if existing request past can be used
+             * Check $requestPath should be unique
              */
-            if ($product->getUrlKey() == '' && !empty($requestPath)
-                && strpos($existingRequestPath, $requestPath) === 0
-            ) {
-                $existingRequestPath = preg_replace(
+            if (isset( $this->_rewrites[ $idPath ] )) {
+                $this->_rewrite = $this->_rewrites[ $idPath ];
+                $existingRequestPath = $this->_rewrites[ $idPath ]->getRequestPath();
+
+                if ($existingRequestPath == $requestPath . $suffix) {
+                    return $existingRequestPath;
+                }
+
+                $existingRequestPath = preg_replace('/' . preg_quote($suffix, '/') . '$/', '', $existingRequestPath);
+                /**
+                 * Check if existing request past can be used
+                 */
+                if ( $product->getUrlKey() == '' && !empty($requestPath)
+                     && strpos($existingRequestPath, $requestPath) === 0 ) {
+                    $existingRequestPath = preg_replace(
                     '/^' . preg_quote($requestPath, '/') . '/', '', $existingRequestPath
-                );
-                if (preg_match('#^-([0-9]+)$#i', $existingRequestPath)) {
-                    return $this->_rewrites[$idPath]->getRequestPath();
+                    );
+                    if (preg_match( '#^-([0-9]+)$#i', $existingRequestPath )) {
+                        return $this->_rewrites[ $idPath ]->getRequestPath();
+                    }
+                }
+
+                $fullPath = $requestPath . $suffix;
+
+                // PATCH TO FIX MAGENTO BUG THAT DUPLICATES REWRITES WHEN URL_KEY IS EQUAL BETWEEN PRODUCTS
+                $pattern = '/^' . preg_quote( $requestPath, '/' ) . '-\d{1,}$/';
+                if (preg_match( $pattern, $existingRequestPath )) {
+                    $fullPath = $existingRequestPath . $suffix;
+                }
+                // END OF PATCH
+
+                if ($this->_deleteOldTargetPath( $fullPath, $idPath, $storeId )) {
+                    return $fullPath;
                 }
             }
+            /**
+             * Check 2 variants: $requestPath and $requestPath . '-' . $productId
+             */
+            $validatedPath = $this->getResource()->checkRequestPaths(
+                array( $requestPath . $suffix, $requestPath . '-' . $product->getId() . $suffix ),
+                $storeId
+            );
 
-            $fullPath = $requestPath.$suffix;
-
-            // PATCH TO FIX MAGENTO BUG THAT DUPLICATES REWRITES WHEN URL_KEY IS EQUAL BETWEEN PRODUCTS
-            $pattern = '/^' . preg_quote($requestPath, '/') . '-\d{1,}$/';
-            if ( preg_match($pattern, $existingRequestPath) ) {
-                $fullPath = $existingRequestPath.$suffix;
+            if ($validatedPath) {
+                return $validatedPath;
             }
-            // END OF PATCH
-
-            if ($this->_deleteOldTargetPath($fullPath, $idPath, $storeId)) {
-                return $fullPath;
-            }
+            /**
+             * Use unique path generator
+             */
+            return $this->getUnusedPath( $storeId, $requestPath . $suffix, $idPath );
         }
-        /**
-         * Check 2 variants: $requestPath and $requestPath . '-' . $productId
-         */
-        $validatedPath = $this->getResource()->checkRequestPaths(
-            array($requestPath.$suffix, $requestPath.'-'.$product->getId().$suffix),
-            $storeId
-        );
-
-        if ($validatedPath) {
-            return $validatedPath;
-        }
-        /**
-         * Use unique path generator
-         */
-        return $this->getUnusedPath($storeId, $requestPath.$suffix, $idPath);
     }
 
-    /**
-     * @param string $requestPath
-     * @param string $idPath
-     * @param int $storeId
-     * @return bool
-     */
-    function _deleteOldTargetPath($requestPath, $idPath, $storeId) {
-        // This function does not exist in older Magento versions.
-        if ( method_exists( get_parent_class($this), '_deleteOldTargetPath' ) ) {
-            return parent::_deleteOldTargetPath($requestPath, $idPath, $storeId);
-        }
-        return false;
-    }
 }
